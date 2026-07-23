f791908816e5551c85fcf68cd5250e7e8b1cf3ad
BaoToast <carey370@gmail.com>
2026-07-23 15:25:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'battle-tracker-145ce72e87';
+const CACHE_NAME = 'battle-tracker-777d8cee25';
 const APP_SHELL = ['./', './index.html'];
 
 self.addEventListener('install', event => {
